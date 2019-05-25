3b280168dc4969913ffd90450cc813c4a66a602c
aleph-naught2tog <maggie@8thlight.com>
2019-05-25 18:51:20 -0500

    Adds Typescript

--- App.tsx
+++ App.tsx
@@ -5,7 +5,9 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        hello world?
+        <Text>
+          ping pong?
+        </Text>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
